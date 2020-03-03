6a1b50e7db8f9b1f8fc5c6f8623c6273a0aa565b
Java <ignacio.riveraherrera@gmail.com>
2020-03-03 13:18:05 -0300

    Pythagorean test

---
+++ java/Pythagorean/Pythagorean.java
@@ -0,0 +1,18 @@
+public class Pythagorean {
+
+    public double calculateHypotenuse(int legA, int legB) {
+
+        
+        
+        double four = (legA*legA)+(legB*legB);
+
+        double squareRoot = Math.sqrt(four);
+
+
+        return squareRoot;
+
+
+    }
+
+
+}
