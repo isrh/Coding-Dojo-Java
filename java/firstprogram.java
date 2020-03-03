f72daf32c01fbd1c3aed5e468ad5e7a2b67a7a92
Java <ignacio.riveraherrera@gmail.com>
2020-03-03 12:19:27 -0300

    firstprogram

---
+++ java/firstprogram.java
@@ -0,0 +1,8 @@
+public class firstprogram{
+    public static void main(String[] args){
+        System.out.println("Mi nombre es Ignacio Rivera");
+        System.out.println("Tengo 25 años de edad");
+        System.out.println("Mi ciudad es Temuco, Región de La Araucanía");
+
+    }
+}
