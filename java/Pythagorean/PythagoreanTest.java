7f04f330139e5e1ff39f15f9f2cab09a65f77e69
isrh <60434201+isrh@users.noreply.github.com>
2020-06-19 13:40:35 -0400

    Delete to update

--- java/Pythagorean/PythagoreanTest.java
+++
@@ -1,9 +0,0 @@
-public class PythagoreanTest{
-    
-    public static void main(String[] args){
-
-        Pythagorean iD= new Pythagorean();
-        double square=iD.calculateHypotenuse(4,3);
-        System.out.println("El valor de la hipotenusa es: " + square);
-    }
-}
